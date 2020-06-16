d323386b4b3a0a2e98d5907f8dea1ebbffc7007d
Yuta Takahashi <yuta-ubiquitous@outlook.jp>
2020-06-16 18:58:27 +0900

    タイマー処理のサンプル

--- index.js
+++ index.js
@@ -2,16 +2,47 @@ const noble = require('@abandonware/noble');
 const config = require('./config.json')
 
 class PikaBreak {
+
+  state = 'stop'
+  packets = []
+  timestamps = []
+
   constructor() {
     console.log('PikaBreak Class')
-    this.state = "stop"
+  }
+
+  discoveredBeacon(_peripheral){
+    if(this.state === 'stop'){
+      this.state = 'found'
+      this.packets.push(_peripheral)
+      const t = new Date().getTime()
+      this.timestamps.push(t)
+
+      // timer start
+      this.startTimer()
+    }
+    else if(this.state === 'found'){
+      this.packets.push(_peripheral)
+      const t = new Date().getTime()
+      this.timestamps.push(t)
+    }
+  }
+
+  async startTimer() {
+    setTimeout(()=>{
+      console.log(this.timestamps)
+
+      this.timestamps = []
+      this.packets = []
+      this.state = 'stop'
+    }, 5000)
   }
 }
 
 const pika = new PikaBreak()
 
 noble.on('stateChange', async (state) => {
-  console.log('state=', state)
+  console.log('state:', state)
   if (state === 'poweredOn') {
     await noble.startScanningAsync([], true);
   }
@@ -21,6 +52,7 @@ noble.on('discover', async (peripheral) => {
   if(peripheral.uuid === config.uuid){
     console.log(peripheral.id, peripheral.rssi)
     console.log(peripheral.advertisement)
+    pika.discoveredBeacon(peripheral)
   }
 });
 
